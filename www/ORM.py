85dfaf8353f8d29e9a810ab7a2a45eaea6b67ee2
jack-hug <46361381@qq.com>
2018-07-26 17:30:40 +0800

    ORM

--- www/ORM.py
+++ www/ORM.py
@@ -1,6 +1,9 @@
-from orm import Model,StringField,IntegerField
 import asyncio,logging
-import aiomysql #导入mysql异步驱动模块
+import aiomysql #导入mysql异步IO驱动模块
+
+#记录sql操作：
+def log(sql,args=[]):
+	logging.info('SQL: %s' % sql)
 
 #创建全局连接池
 @asyncio.coroutine
@@ -41,23 +44,104 @@ def select(sql,args,size = None):
 def execute(sql,args):
 	log(sql)
 	with (yield from __pool) as conn:
+		if not autocommit:
+			yield from conn.begin()
 		try:
 			cur = yield from conn.cursor()
 			yield from cur.execute(sql.replace('?','%s'),args)
 			affected = cur.rowcount()
+			if not autocommit:
+				yield from conn.commit() 
 			yield from cur.close()
 		except BaseException as e:
+			if not autocommit:
+				yield from conn.roll.back()
 			raise
 		return affected
 
-class User(Model):
-	__table__ = 'users'
+#制作参数字符串
+def create_args_string(num):
+	L = []
+	for n in range(num):
+		L.append('?')
+	return ','.join(L)
 
-	id = IntegerField(primary_key = True)
-	name = StringField()
+#Field类 定义数据类型的基类
+class Field(object):
+	def __init__(self, name,column_type,primary_key,default):
+		self.name = name
+		self.column_type = column_type
+		self.primary_key = primary_key
+		self.default = default
+	def __str__(self):
+		return '<%s,%s: %s>' % (self.__class__.__name__,self.column_type,self.name)
 
+#StringField类 定义字符类，默认值为100
+class StringField(Field):
+	def __init__(self, name = None,primary_key = False,default = None,ddl = 'Varchar(100)'):
+		super().__init__(name,ddl,primary_key,default)
+
+#继承Field，定义boolean类：
+class BooleanField(Field):
+	def __init__(self, name = None,default = False):
+		super().__init__(name,'boolean',False,default)
+
+#继承Field，定义整数类(bigint)，默认值为0
+class IntegerField(Field):
+	def __init__(self, name = None,primary_key = False,default = 0):
+		super().__init__(name,'bigint',primary_key,default)
+
+#继承Field，定义浮点类
+class FloadField(Field):
+	def __init__(self, name = None,primary_key = False,default = 0):
+		super().__init__(name,'real',primary_key,default)
+		
+#继承Field，定义text类
+class TextField(Field):
+	def __init__(self, name = None,default = None):
+		super().__init__(name,'text',False,default)
+
+#ModelMetaclass
+class ModelMetaclass(type):
+	def __new__(cls,name,bases,attrs):
+		#排除Model本身
+		if name == 'Model':
+			return type.__new__(cls,name,bases,attrs)
+		#获取TABLE名称
+		tableName = attrs.get('__table__',None) or name
+		logging.info('found model:%s (table:%s)' % (name,tableName))
+		#获取所有的Field和主键名
+		mappings = dict()
+		primaryKey = None
+		fields = []
+		for k,v in attrs.items():
+			if isinstance(v,Field):
+				logging.info('  found mapping: %s ==> %s' % (k,v))
+				mappings[k] = v
+				if v.primary_key:
+					if primaryKey:
+						raise RuntimeError('Duplicate primary key for field: %s' % k)
+					primaryKey = k
+				else:
+					fields.append(k)
+			if not primaryKey:
+				raise RuntimeError('Primary key not found.')
+			for k in mappings.keys():
+				attrs.pop(k)
+			escaped_fields = list(map(lambda f: ' `%s`' % f,fields))
+			attrs['__mappings__'] = mappings #保存属性和列的映射关系
+			attrs['__table__'] = tableName
+			attrs['__primary_key__'] = primaryKey #主键属性名
+			attrs['__fields__'] = fields #除主键外的属性名
+			#构造默认的SELECT,INSERT,UPDATE和DELETE语句：
+			attrs['__select__'] = 'select `%s`,%s from `%s`' % (primaryKey,','.join(escaped_fields),tableName)
+			attrs['__insert__'] = 'insert into `%s` (%s,`%s`) values (%s)' % (tableName,','.join(escaped_fields),primaryKey,create_args_string(len(escaped_fields)+1))
+			attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName,','.join(map(lambda f:'`%s`=?' % (mappings.get(f).name or f),fields)),primaryKey)
+			attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName,primaryKey)
+			return type.__new__(cls,name,bases,attrs)
+
+#定义所有ORM映射的基类Model
 class Model(dict,metaclass = ModelMetaclass):
-	"""docstring for Model"""
 	def __init__(self, **kw):
 		super(Model, self).__init__(**kw)
 
@@ -83,3 +167,74 @@ class Model(dict,metaclass = ModelMetaclass):
 				setattr(self,key,value)
 		return value
 
+	@classmethod
+	@asyncio.coroutine
+	def find(cls,pk):
+		' find object by primary key.'
+		rs = yield from select('%s where `%s`=?' % (cls.__select__,cls.__primary_key__),[pk],1)
+		if len(rs) == 0:
+			return None
+		return cls(**rs[0])
+
+	@asyncio.coroutine
+	def save(self):
+		args = list(map(self.getValueOrDefault,self.__fields__))
+		args.append(self.getValueOrDefault(self,__primary_key__))
+		rows = yield from execute(self.__insert__,args)
+		if rows != 1:
+			logging.warn('failed to insert record:affected rows: %s' % rows)
+
+
+	@asyncio.coroutine
+	def update(self):
+		args = list(map(self.getValue,self.__fields__))
+		args.append(self.getValue(self.__primary_key__))
+		rows = yield from execute(self.__update__,args)
+		if rows != 1:
+			logging.warn('failed to update by primary key: affected rows: %s' % rows)
+
+	@asyncio.coroutine
+	def remove(self):
+		args = [self.getvalue(self.__primary_key__)]
+		rows = yield from execute(self.__delete__,args)
+		if rows != 1:
+			logging.warn('failed to remove by primary key:affected rows: %s' % rows)
+
+	@classmethod
+	@asyncio.coroutine
+	def findAll(cls,where = None,args = None,**kw):
+		sql = [cls.__select__]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		if args is None:
+			args = []
+		orderBy = k.get('orderBy',None)
+		if orderBy:
+			sql.append('order by')
+			sql.append(orderBy)
+		limit = kw.get('limit',None)
+		if limit is not None:
+			sql=append('limit')
+			if isinstance(limit,ini):
+				sql.append('?')
+				sql.append(limit)
+			elif isinstance(limit,tuple) and len(limit) == 2:
+				sql.append('?,?')
+				args.extend(limit)
+			else:
+				raise ValueError('Invalid limit value: %s' % str(limit))
+		rs = yield from select(' '.join(sql),args)
+		return [cls(**r) for r in rs]
+
+	@classmethod
+	@asyncio.coroutine
+	def findNumber(cls,selectField,where = None,args = None):
+		sql = ['select %s _num_ from `%s`' % (selectField,cls.__table__)]
+		if where:
+			sql.append('where')
+			sql.append(where)
+		rs = yield from select(' '.join(sql),args,1)
+		if len(rs) == 0:
+			return None
+		return rs[0]['_num_']
