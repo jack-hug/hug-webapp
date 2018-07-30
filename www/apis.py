0e79e16ecb792cb8ddf2b4e628453a5b25501334
jack-hug <46361381@qq.com>
2018-07-30 17:22:52 +0800

    用户注册和登登录

--- www/apis.py
+++ www/apis.py
@@ -6,3 +6,12 @@ class APIError(Exception):
 		self.error = error
 		self.data = data
 		self.message = message
+
+class APIValueError(APIError):
+	def __init__(self, field,message=''):
+		super(APIValueError, self).__init__('value:invalid',field,message)
+
+class APIResourceNotFoundError(APIError):
+	def __init__(self, message=''):
+		super(APIResourceNotFoundError, self).__init__('permission:forbidden','permission',message)
+			
--- www/config_default.py
+++ www/config_default.py
@@ -1,11 +1,11 @@
-config = {
+configs = {
 	'db':{
 		'host':'127.0.0.1',
 		'port':3306,
 		'user':'root',
 		'password':'HUANGzeng123',
 		'database':'awesome'
-	}
+	},
 	'session':{
 		'secret':'AwEsOmE'
 	}
